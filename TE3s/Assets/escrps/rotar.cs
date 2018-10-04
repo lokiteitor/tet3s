49b336bcc583207c2b1ef6fbad201a4588f553ba
Dant117 <39505534+Dant117@users.noreply.github.com>
2018-10-04 13:05:54 -0500

    rotar
    script rotar

---
+++ TE3s/Assets/escrps/rotar.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotar : MonoBehaviour {
+    public GameObject figura;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("s"))
+            figura.transform.Rotate(Vector3.right * 90);
+	}
+}

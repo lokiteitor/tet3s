9cb7539bbd8a6487ac57d8ca00ade1b63ca45b09
Dant117 <39505534+Dant117@users.noreply.github.com>
2018-10-04 13:36:47 -0500

    rotar incompleto
    arriba

---
+++ TE3s/Assets/escrps/rotarBase.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotarBase : MonoBehaviour {
+    public GameObject bas;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("q")) {
+            float angulo= bas.transform.rotation.eulerAngles.z -90;
+                while (bas.transform.rotation.eulerAngles.z >= angulo)
+                    bas.transform.Rotate(-Vector3.forward * Time.deltaTime);
+        }
+        /*if (Input.GetKeyDown("q"))
+            bas.transform.Rotate(-Vector3.up * Time.deltaTime);*/
+    }
+}

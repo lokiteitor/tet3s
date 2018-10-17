35ac9ebde999a7f36f581a7de10c8dad9111c6ec
David Delgado <lokiteitor.delgado@gmail.com>
2018-10-17 01:51:53 -0500

    Revert "Merge branch 'master' of https://github.com/lokiteitor/tet3s"
    This reverts commit f5f0dc937a8cafbb437bc2bcddfa2e73af7e47b1, reversing
    changes made to ed9a22f04be56861aad4b7aba1e8f8440712604a.

---
+++ TE3s/Assets/escrps/Scenas/Fondo.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fondo : MonoBehaviour
+{
+    public Material fondo1;
+    public Material fondo2;
+    public Material fondo3;
+    public Material fondo4;
+    public GameObject plano;
+    private Material[] fondos;
+    private int index = 0;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        fondos = new Material[] { fondo1, fondo2, fondo3, fondo4 };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Renderer render = plano.GetComponent(typeof(Renderer)) as Renderer;
+            if (render != null)
+            {
+                render.sharedMaterial = fondos[this.index];
+                this.index += 1;
+                Debug.Log("entro");
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Renderer render = plano.GetComponent(typeof(Renderer)) as Renderer;
+            if (render != null)
+            {
+                render.sharedMaterial = fondos[this.index];
+                this.index -= 1;
+                Debug.Log("entro");
+            }
+        }
+
+        if (this.index >= 4)
+        {
+            this.index = 0;
+        }
+        else if (this.index <= -1)
+        {
+            this.index = 3;
+        }
+
+    }
+}
